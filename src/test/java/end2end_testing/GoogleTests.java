3976dd98ce305bda1793541ba854fd368da288a0
KareemZafan <kareemm.zafan@gmail.com>
2022-11-12 21:04:05 +0200

    Start e2e Testing

---
+++ src/test/java/end2end_testing/GoogleTests.java
@@ -0,0 +1,44 @@
+package end2end_testing;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class GoogleTests {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testGoogleLogo() {
+        driver.get("https://www.google.com/");
+        boolean actResult = driver.findElement(By.cssSelector("img.lnXdpd")).isDisplayed();
+        Assert.assertTrue(actResult);
+    }
+
+    @Test
+    public void testSearchResults() {
+        driver.findElement(By.name("q")).sendKeys("ITI Pluralsight Scholarship", Keys.ENTER);
+        String text = driver.findElement(By.cssSelector("div#result-stats")).getText();
+        System.out.println(text);
+        Assert.assertNotEquals(text, "");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
